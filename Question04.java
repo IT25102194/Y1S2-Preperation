bda010c574297431e6a22a167b304403bbd735f7
IT25102194 <it25102194+primary@my.sliit.lk>
2026-02-08 19:43:47 +0530

    Question04.java

---
+++ Question04.java
@@ -0,0 +1,16 @@
+class Question04 {
+    public static void main(String[] args) {
+        int rows = 5;
+        int columns = 5;
+        
+        while (rows > 0) {
+            while (columns > 0) {
+                System.out.print("* ");
+                columns = columns - 1;
+            }
+            System.out.print("\n");
+            columns = 5;
+            rows = rows - 1;
+        }
+    }
+}
